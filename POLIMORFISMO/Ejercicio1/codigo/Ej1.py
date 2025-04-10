fc58ac4ec985b569eac20d3cc645655803b74c6b
Ronald Paul Gutierrez Mamani <160867694+Ronald0304@users.noreply.github.com>
2025-04-10 18:35:11 -0400

    Update Ej1.py

--- POLIMORFISMO/Ejercicio1/codigo/Ej1.py
+++ POLIMORFISMO/Ejercicio1/codigo/Ej1.py
@@ -1,10 +1,15 @@
+#1.Sea la clase Videojuego:
+#a)Instanciar al menos 2 videojuegos
+#b)Sobrecargar el constructor 2 veces
+#c)Implementar un método mostrar()
+#d)Sobrecargar el método agregarJugadores() donde en el primero se agregue solo 1 jugador y en otro se ingrese una cantidad de jugadores a aumentar.
 from multimethod import multimethod
 
 class Videojuego:
     def __init__(self, nombre="Desconocido", plataforma="Desconocida", cantidadaJugadores=0):
-        self.nombre = nombre
-        self.plataforma = plataforma
-        self.cantidadaJugadores = cantidadaJugadores
+        self.__nombre = nombre
+        self.__plataforma = plataforma
+        self.__cantidadaJugadores = cantidadaJugadores
 
     @classmethod
     def desdeNombre(cls, nombre):
@@ -15,17 +20,17 @@ class Videojuego:
         return cls(nombre, plataforma)
 
     def mostrar(self):
-        print(f"Nombre: {self.nombre}")
-        print(f"Plataforma: {self.plataforma}")
-        print(f"Cantidad de Jugadores: {self.cantidadaJugadores}")
+        print(f"Nombre: {self.__nombre}")
+        print(f"Plataforma: {self.__plataforma}")
+        print(f"Cantidad de Jugadores: {self.__cantidadaJugadores}")
 
     @multimethod
     def agregarJugadores(self):
-        self.cantidadaJugadores += 1
+        self.__cantidadaJugadores += 1
 
     @multimethod
     def agregarJugadores(self, cantidad: int):
-        self.cantidadaJugadores += cantidad
+        self.__cantidadaJugadores += cantidad
 
 v1 = Videojuego.desdeNombreYPlataforma("FIFA 24", "PlayStation 5")
 v2 = Videojuego.desdeNombre("Minecraft")
